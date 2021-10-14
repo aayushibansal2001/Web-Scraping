d1a80feff5e085f2c3f9fbfaedfdce57ca61084e
Aayushi Bansal <51708118+aayushibansal2001@users.noreply.github.com>
2021-10-14 20:22:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,16 @@
-import bs4
-
-
-# Step 0: Install all the requirements in terminal
-# pip install requests
-# pip install bs4
-# pip install html5lib
-
 import requests
 from bs4 import BeautifulSoup
-url = "https://www.codewithharry.com"
+url = "https://mlh.io/seasons/2021/events"
 
-# Step 1: Get the HTML
 r = requests.get(url)
 htmlContent = r.content
 
-# Step 2: Parse the HTML
 soup = BeautifulSoup(htmlContent, 'html.parser')
-# print(soup.prettify)
 
-# Step 3: HTML Tree traversal
-
-# Commonly used types of Objects:
-# print(type(title)) # 1. Tag
-# print(type(title.string)) # 2. NavigableString
-# print(type(soup)) # 3. BeautifulSoup
-
-# Get the title of the HTML page
 title = soup.title
-print(title)
+print(title.text)
 
-# Get all paragraphs from the page
-paras = soup.find_all('p')
-print(paras)
+events = soup.find_all("a", class_ = "event-link")
 
-# Get first para element in the HTML page
-print(soup.find('p')) 
-
-# Get classes of any element in the HTML Page
-print(soup.find('p')['class'])
-
-# Find all elements whose class is lead
-print(soup.find_all("p", class_ = "lead"))
-
-# Get the text from the tags/soup
-print(soup.find('p').get_text())
-
-# Get all the anchor tags from the page
-anchors = soup.find_all('a')
-
-all_links = set() # making a set so that we don't get repeated links
-
-# Get all the links on the page
-for link in anchors:
-    if(link.get('href') != '#'):
-        linkText = "https://codewithharry.com" + link.get('href')
-        all_links.add(linkText)
-        print(linkText)
-
-    
+for event in events:
+    print(event['title'] + ':' + event['href'])
